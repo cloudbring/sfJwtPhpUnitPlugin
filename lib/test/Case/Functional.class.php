70806bbccfdfc8acfd79a239c64670a4956417a9
pzerin <pzerin@7d584379-5e2d-0410-ba1a-e2d590f5d264>
2010-08-04 18:47:19 +0000

    Added Test_Case_Functional->assertForwardedTo() and ->assertRedirectedTo(), refactored magic strings.

--- lib/test/Case/Functional.class.php
+++ lib/test/Case/Functional.class.php
@@ -10,6 +10,14 @@
  */
 abstract class Test_Case_Functional extends Test_Case
 {
+  const
+    MSG_STATUSCODE      = 'Browser returned HTTP status %d.',
+    MSG_FORWARD_MODULE  = 'Browser is forwarded to %s module.',
+    MSG_FORWARD_ACTION  = 'Browser is forwarded to %s action.',
+    MSG_REDIRECT_MODULE = 'Browser is redirected to %s module.',
+    MSG_REDIRECT_ACTION = 'Browser is redirected to %s action.',
+    MSG_REDIRECT_RAW    = 'Browser is redirected to URL: %s.';
+
   protected
     $_browser;
 
@@ -33,9 +41,90 @@ abstract class Test_Case_Functional extends Test_Case
   {
     if( $msg === null )
     {
-      $msg = 'HTTP response status is ' . $code . '.';
+      $msg = sprintf(self::MSG_STATUSCODE, $code);
     }
 
     $this->assertEquals($code, $this->_browser->getStatusCode(), $msg);
+  }
+
+  /** Asserts that the browser has been forwarded (not redirected) to the
+   *   specified action.
+   *
+   * @param string                $module
+   * @param string                $action
+   * @param string|array(string)  $msg    Custom failure message(s) (optional).
+   *  If an array is provided, the first element will be the message for the
+   *   module check, and the second element will be the message for the action
+   *   check.
+   *
+   * @return void
+   */
+  public function assertForwardedTo( $module, $action, $msg = null )
+  {
+    if( $msg === null )
+    {
+      $msg = array(
+        sprintf(self::MSG_FORWARD_MODULE, $module),
+        sprintf(self::MSG_FORWARD_ACTION, $action)
+      );
+    }
+    elseif( ! is_array($msg) )
+    {
+      $msg = array($msg, $msg);
+    }
+
+    $Entry = $this->_browser->getContext()->getActionStack()->getLastEntry();
+
+    $this->assertEquals($module, $Entry->getModuleName(), reset($msg));
+    $this->assertEquals($action, $Entry->getActionName(), next($msg));
+  }
+
+  /** Asserts that the browser has been redirected (not forwarded) to the
+   *   specified action.
+   *
+   * @param string                $module
+   * @param string                $action If null, $module is considered to be a
+   *  URL (relative or absolute).
+   * @param string|array(string)  $msg    Custom failure message(s) (optional).
+   *  If an array is provided, the first element will be the message for the
+   *   module check, and the second element will be the message for the action
+   *   check.
+   *
+   * @return void
+   */
+  public function assertRedirectedTo( $module, $action = null, $msg = null )
+  {
+    $destination = $this->_browser->getResponse()->getHttpHeader('location');
+
+    if( $action )
+    {
+      if( $msg === null )
+      {
+        $msg = array(
+          sprintf(self::MSG_REDIRECT_MODULE, $module),
+          sprintf(self::MSG_REDIRECT_ACTION, $action)
+        );
+      }
+      elseif( ! is_array($msg) )
+      {
+        $msg = array($msg, $msg);
+      }
+
+      list($route, $params) =
+        $this->_browser->getContext()->getController()
+          ->convertUrlStringToParameters($destination);
+
+      $this->assertEquals($module, $params['module'], reset($msg));
+      $this->assertEquals($action, $params['action'], next($msg));
+    }
+    else
+    {
+      if( $msg === null )
+      {
+        $msg = sprintf(self::MSG_REDIRECT_RAW, $module);
+      }
+
+      $this->assertEquals($module, $destination, $msg);
+    }
   }
 }
