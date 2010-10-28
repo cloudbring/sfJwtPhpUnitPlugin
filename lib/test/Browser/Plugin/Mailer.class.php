960cf0f6682811805aa382a48e6e077a706ce879
pzerin <pzerin@7d584379-5e2d-0410-ba1a-e2d590f5d264>
2010-10-28 18:48:32 +0000

    Added Test_Browser_Plugin_Mailer->getMessage() and getMessagesMatching() (experimental).

--- lib/test/Browser/Plugin/Mailer.class.php
+++ lib/test/Browser/Plugin/Mailer.class.php
@@ -33,4 +33,47 @@ class Test_Browser_Plugin_Mailer extends Test_Browser_Plugin
 
     return $this;
   }
+
+  /** Accesses a specific message from the queue.
+   *
+   * @param int $pos
+   *
+   * @return Swift_Message|null
+   */
+  public function getMessage( $pos )
+  {
+    $messages = $this->getMessages();
+    return isset($messages[$pos]) ? $messages[$pos] : null;
+  }
+
+  /** Retrieve the messages matching a given field value.
+   *
+   * @param string $field
+   * @param string $value
+   *
+   * @return array(Swift_Message)
+   */
+  public function getMessagesMatching( $field, $value )
+  {
+    $messages = array();
+
+    /* @var $Message Swift_Message */
+    foreach( $this->getMessages() as $Message )
+    {
+      if( $headers = $Message->getHeaders()->getAll($field) )
+      {
+        /* @var $Header Swift_Mime_Header */
+        foreach( $headers as $Header )
+        {
+          if( $Header->getFieldBody() == $value )
+          {
+            $messages[] = $Message;
+            break;
+          }
+        }
+      }
+    }
+
+    return $messages;
+  }
 }
