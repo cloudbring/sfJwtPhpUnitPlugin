c9fbe27eda2a95e7b05889483ec04c0dab605784
pzerin <pzerin@7d584379-5e2d-0410-ba1a-e2d590f5d264>
2011-05-02 15:22:46 +0000

    Added MIT license and @author text to .php files to match formatting of other JWT OSS project files.

--- lib/test/Browser/Plugin/ViewCache.class.php
+++ lib/test/Browser/Plugin/ViewCache.class.php
@@ -1,5 +1,29 @@
 <?php
+/**
+ * Copyright (c) 2011 J. Walter Thompson dba JWT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /** Extends browser view cache functionality.
+ *
+ * @author Phoenix Zerin <phoenix.zerin@jwt.com>
  *
  * @package sfJwtPhpUnitPlugin
  * @subpackage lib.test.browser.plugin
@@ -64,12 +88,12 @@ class Test_Browser_Plugin_ViewCache extends Test_Browser_Plugin
 
     return false;
   }
-  
+
   /** Returns the HTML content of a cached URI.
-   * 
+   *
    * @param string $uri If not specified, the browser object's current URI
    *  (cache key) will be used.
-   * 
+   *
    * @return string
    */
   public function getContent( $uri )
@@ -78,7 +102,7 @@ class Test_Browser_Plugin_ViewCache extends Test_Browser_Plugin
     {
       $uri = $this->getCurrentCacheKey();
     }
-    
+
     return $this->has($uri) ? unserialize($this->get($uri))->getContent() : '';
   }
 }
